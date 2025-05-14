4d5edf4c8adc436b64ce7732fabbcd0d34f1dd05
Burhan Cavdaroglu <burhancavdaroglu70@gmail.com>
2025-05-14 16:03:19 +0300

    establish cli and main different folder

--- cli/main.py
+++ cli/main.py
@@ -1,28 +1,8 @@
-from typing import Optional
+import cli 
+from __init__ import __app_name__
 
-import typer
-
-from __init__ import __app_name__, __version__
-
-app = typer.Typer()
-
-def _version_callback(value: bool) -> None:
-    if value:
-        typer.echo(f"{__app_name__} v{__version__}")
-        raise typer.Exit()
-
-@app.callback()
-def main(
-    version: Optional[bool] = typer.Option(
-        None,
-        "--version",
-        "-v",
-        help="Show the application's version and exit.",
-        callback=_version_callback,
-        is_eager=True,
-    )
-) -> None:
-    return
+def main():
+    cli.app(prog_name = __app_name__)
 
 if __name__ == "__main__":
-    app()
+    main()
