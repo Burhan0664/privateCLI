e7a4e435176b15dc986bcfdab32c7034517db2f1
Burhan Cavdaroglu <burhancavdaroglu70@gmail.com>
2025-05-21 16:28:59 +0300

    fix bug

--- cli/cli.py
+++ cli/cli.py
@@ -2,14 +2,21 @@ from typing import Optional
 
 import typer
 
-from cli.__init__ import __app_name__, __version__
+from __init__ import __app_name__, __version__
+
 
 app = typer.Typer()
-
 def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
+
+@app.command()
+def githubInformation(name:str , password:str):
+    print(f"Github name : {name}")
+    print(f"Github password : {password}")
+    typer.echo(f"Welcome to my CLI {name}")
+    # get the data from db and match input parameters
 
 @app.callback()
 def main(
@@ -17,10 +24,12 @@ def main(
         None,
         "--version",
         "-v",
-        "-vers",
+        "--vers",
         help="Show the application's version and exit.",
         callback=_version_callback,
         is_eager=True,
     )
 ) -> None:
+    
+
     return
